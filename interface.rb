469f531d6ca206062d4f47324d6ffa38c17f5095
Enrique Ramirez <nrqrmz@gmail.com>
2023-06-02 16:50:19 -0500

    set logic for users

---
+++ interface.rb
@@ -0,0 +1,12 @@
+require_relative 'game'
+
+puts 'Welcome'
+puts "Let's play Tic, Tac, Toe"
+puts 'Player 1, please enter your name:'
+print '> '
+player1 = Player.new(gets.chomp.capitalize, 'X')
+puts 'Player 2, please enter your name:'
+print '> '
+player2 = Player.new(gets.chomp.capitalize, 'O')
+game = Game.new(player1, player2)
+game.play
